a60b29ea940c9c527ee641202d0f9121415df179
gani29-tech <ganesh.m@techouts.co.in>
2026-01-02 18:52:09 +0530

    Added Module Package Demo File

---
+++ com/techouts/cj5/ModulePackageDemo.java
@@ -0,0 +1,27 @@
+package com.techouts.cj5;
+//module-info.java
+
+//  module com.techouts.cj5{
+//      exports com.techouts.cj5;
+//  }
+
+//public class Test{
+//    public static void greeting(){
+//        System.out.println("hello");
+//    }
+//}
+
+
+//module-info.java(client)
+
+//   module com.techouts.app{
+//       requires com.techouts.cj5;
+//   }
+
+//Client.java
+//import com.techouts.cj5.Test;
+//public class ModulePackageDemo {
+//    public static void main(String[] args) {
+//        Test.greeting();
+//    }
+//}
