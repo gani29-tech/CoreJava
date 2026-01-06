ac3077ce2977044b37ca4f89e20cffaf57e0bd7c
gani29-tech <ganesh.m@techouts.co.in>
2026-01-06 18:01:31 +0530

    Updated Arrays.Ex with 3D array

--- com/techouts/cj3/ArraysEx.java
+++ com/techouts/cj3/ArraysEx.java
@@ -4,34 +4,51 @@ import java.util.Scanner;
 public class ArraysEx {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] array1 = new int[3];
-        for(int a = 0; a < array1.length; a++){
-            array1[a] = input.nextInt();
-        }
-        int[] array2 = {1,2,3,4,5};
-        for (int k : array1) {
-            System.out.print(k + " ");
-        }
-        System.out.println();
-        for (int j : array2) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
-        int[][] array3 = {{1,2,3,4},{5,6,7,8}};
-        for (int a = 0; a < array3.length; a++){
-            for(int b = 0; b < array3[a].length; b++){
-                System.out.print(array3[a][b] + " ");
+//        int[] array1 = new int[3];
+//        for(int a = 0; a < array1.length; a++){
+//            array1[a] = input.nextInt();
+//        }
+//        int[] array2 = {1,2,3,4,5};
+//        for (int k : array1) {
+//            System.out.print(k + " ");
+//        }
+//        System.out.println();
+//        for (int j : array2) {
+//            System.out.print(j + " ");
+//        }
+//        System.out.println();
+//        int[][] array3 = {{1,2,3,4},{5,6,7,8}};
+//        for (int a = 0; a < array3.length; a++){
+//            for(int b = 0; b < array3[a].length; b++){
+//                System.out.print(array3[a][b] + " ");
+//            }
+//        }
+//        int[][] array4 = new int[3][2];
+//        for (int a = 0; a < array4.length; a++){
+//            for(int b = 0; b < array4[a].length; b++){
+//                array4[a][b] = input.nextInt();
+//            }
+//        }
+//        for (int a = 0; a < array4.length; a++){
+//            for(int b = 0; b < array4[a].length; b++){
+//                System.out.print(array4[a][b] + " ");
+//            }
+//            System.out.println();
+//        }
+        int[][][] array5 = new int[3][3][4];
+        for (int a = 0; a < array5.length; a++){
+            for(int b = 0; b < array5[a].length; b++){
+                for(int c = 0; c < array5[a][b].length; c++){
+                    array5[a][b][c] = input.nextInt();
+                }
             }
         }
-        int[][] array4 = new int[3][2];
-        for (int a = 0; a < array4.length; a++){
-            for(int b = 0; b < array4[a].length; b++){
-                array4[a][b] = input.nextInt();
-            }
-        }
-        for (int a = 0; a < array4.length; a++){
-            for(int b = 0; b < array4[a].length; b++){
-                System.out.print(array4[a][b] + " ");
+        for (int a = 0; a < array5.length; a++){
+            for(int b = 0; b < array5[a].length; b++){
+                for(int c = 0; c < array5[a][b].length; c++){
+                    System.out.print(array5[a][b][c] + " ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
