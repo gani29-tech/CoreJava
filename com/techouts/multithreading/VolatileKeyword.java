d8470c171db23ede83a16ddb753868d9db633720
gani29-tech <ganesh.m@techouts.co.in>
2026-01-09 11:26:16 +0530

    Updated Volatile

--- com/techouts/multithreading/VolatileKeyword.java
+++ com/techouts/multithreading/VolatileKeyword.java
@@ -1,47 +1,32 @@
     package com.techouts.multithreading;
 
-    import java.util.logging.Level;
     import java.util.logging.Logger;
 
-    class VolatileData{
-        private volatile int counter = 0;
+    class VolatileKeyword extends Thread{
+        private volatile boolean flag=true;
+        public static final Logger logger = Logger.getLogger(VolatileKeyword.class.getName());
 
-        public int getA() {
-            return counter;
-        }
-
-        public void incrementA() {
-            counter=counter+1;
-        }
-    }
-    class VolatileExample extends Thread{
-        private static final Logger logger = Logger.getLogger(VolatileExample.class.getName());
-        private final VolatileData data;
-        public VolatileExample(VolatileData data){
-            this.data=data;
-        }
         @Override
         public void run() {
-            int oldValue= data.getA();
-            logger.log(Level.INFO,"Thread - {0} oldValue is {1}",new Object[]{Thread.currentThread().threadId(),oldValue});
-            data.incrementA();
-            int  newValue= data.getA();
-            logger.log(Level.INFO,"Thread - {0} new value is {1}",new Object[]{Thread.currentThread().threadId(),newValue});
+            while (flag) {
+                logger.info("Running");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        public void stopRun(){
+            logger.info("Stopping");
+            flag=false;
         }
     }
-       public class VolatileKeyword {
-        private static final int NO_OF_THREADS = 3;
+    class VolatileExample {
         public static void main(String[] args) throws InterruptedException {
-            VolatileData data=new VolatileData();
-            Thread[] threads=new Thread[NO_OF_THREADS];
-            for(int i = 0; i< NO_OF_THREADS; i++){
-                threads[i]=new VolatileExample(data);
-            }
-            for(int i = 0; i< NO_OF_THREADS; i++){
-                threads[i].start();
-            }
-            for(int i = 0; i< NO_OF_THREADS; i++){
-                threads[i].join();
-            }
+            VolatileKeyword vk=new VolatileKeyword();
+            vk.start();
+            Thread.sleep(200);
+            vk.stopRun();
         }
     }
