d9a283656832c467f8a8db377b3599bba552c8be
gani29-tech <ganesh.m@techouts.co.in>
2026-01-12 17:01:09 +0530

    Updated with available,mark and reset methods

--- com/techouts/io/BufferedInputStreamEx.java
+++ com/techouts/io/BufferedInputStreamEx.java
@@ -8,8 +8,17 @@ import static com.techouts.io.FileInputStreamEx.filename;
 
 public class BufferedInputStreamEx {
     public static void main(String[] args) throws IOException {
-        try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filename))){
+        try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filename),4)){
             int data;
+            System.out.println(bis.markSupported());
+            bis.mark(5); // Marks the starting position
+            System.out.println(bis.available()); //checks the available size to be read
+            for(int i=0;i<10;i++){
+                data = bis.read();
+                System.out.print((char) data);
+            }
+            System.out.println(bis.available());
+            bis.reset(); // it reset to marked position
             while((data=bis.read())!=-1){
                 System.out.print((char) data);
             }
